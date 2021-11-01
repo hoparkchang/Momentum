20dc2a39be713ee2a95c463585c37794a348df1b
arkchangho <2sighj@gmail.com>
2021-11-01 23:15:20 +0900

    first

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert("hi");
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", 'sat'];
+
+//Get Item from Array
+console.log(daysOfWeek);
+
+// Add one more dayt to the array
+daysOfWeek.push('sun');
+
+console.log(daysOfWeek);
