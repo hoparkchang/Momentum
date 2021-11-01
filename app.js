917ea6edf2f93eed6f51d2dc980ce5ecc073640e
arkchangho <2sighj@gmail.com>
2021-11-01 23:28:29 +0900

    commit on "main"

--- app.js
+++ app.js
@@ -1,9 +1,2 @@
-const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", 'sat'];
-
-//Get Item from Array
-console.log(daysOfWeek);
-
-// Add one more dayt to the array
-daysOfWeek.push('sun');
-
-console.log(daysOfWeek);
+const toBuy = ["potato", "tomato", "pizza"];
+toBuy.push("kimbab");
